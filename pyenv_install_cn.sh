5246675fdcd90fab0ad88aaae0869aaaba0ce7b6
D_catch <429249686@qq.com>
2017-02-23 14:30:41 +0800

    create the first version

---
+++ pyenv_install_cn.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+#auth:d_catch
+#for the people in gfw
+#using souhu source
+#
+pyenv install --list
+
+echo 'wich version do you need'
+read version
+echo "input your pyenv's name:"
+read version_name
+wget http://mirrors.sohu.com/python/$version/Python-$version.tar.xz -P ~/.pyenv/cache/;
+pyenv install $version
+echo
+echo "$version installed"
+echo 'now active the version ,now '
+
+pyenv virtualenv $version $version_name
+
+pyenv activate $version_name
+
+echo
+echo "begin the pyenv journey"
